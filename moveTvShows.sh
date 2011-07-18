b7948f7f32a591a12c7764ddfdd4d0dbaf552e7b
elm <elm@skweez.net>
2011-07-18 22:57:45 +0200

    Handle files with whitespaces in the name

--- moveTvShows.sh
+++ moveTvShows.sh
@@ -18,15 +18,15 @@ while read filename; do
                 continue
         fi
 
-        # First replace all _ with . to get consistent filenames
-        newFilename=`echo $filename | sed 's/_/./g'`
+        # First replace all _ and whitespace chars with . to get consistent filenames
+        newFilename=`echo "$filename" | sed 's/[_ ]/./g' | sed 's/\.\W\././g'`
         newPath=`echo $newFilename | sed 's/^\([a-zA-Z.]*\)\.[S|[:digit:]].*/\1/'| tr -s '.' ' '`
         logger -t $0 Moving $filename to $TVSHOWS_FOLDER/$newPath/$newFilename
         if [ ! -d "$TVSHOWS_FOLDER/$newPath" ]; then
                 logger -t $0 $TVSHOWS_FOLDER/$newPath does not exist. Creating it...
                 mkdir -p "$TVSHOWS_FOLDER/$newPath"
         fi
-        mv $filename "$TVSHOWS_FOLDER/$newPath/$newFilename"
+        mv "$filename" "$TVSHOWS_FOLDER/$newPath/$newFilename"
 done
 
 logger -t $0 Done
